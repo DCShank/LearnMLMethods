2359244ce2cf7b06810cd51315569ff98147ab9e
David Shank <shank.david@gmail.com>
2017-01-26 21:07:46 -0500

    Data is now mostly parsed. Currently NaNing at bayes

--- naive-bayes/src/Main.hs
+++ naive-bayes/src/Main.hs
@@ -20,13 +20,110 @@ baseballStats = decode NoHeader
 main :: IO ()
 main = do
     csvData <- BL.readFile "batting.csv"
-    let summed = F.foldr summer 0 (baseballStats csvData)
-    let sums = F.foldr mapper M.empty (baseballStats csvData)
-    putStrLn $ "Total atBats was: " ++ (show summed)
-    putStrLn $ "Maps" ++ (show sums)
+    --let summed = F.foldr summer 0 (baseballStats csvData)
+    let sums = F.foldr mapper M.empty (baseballStats csvData) ::
+                            M.Map BL.ByteString (Int, [(Integer,Integer)])
+    let total = foldr totaler 0 sums
+    let list = M.toList sums
+    let cleanList = fmap cleanTuple list
+    putStrLn $ "stuffs " ++ (show cleanList)
+    let curriedStat = statify (fromIntegral total)
+    let statList = map curriedStat cleanList
+    let cls = bayes statList [1]
+    let normFolded = fmap (normFold [50]) statList
+    putStrLn $ "after norming " ++ (show normFolded)
+    
+    --putStrLn $ "Total atBats was: " ++ (show summed)
+    putStrLn $ "team was" ++ (show cls)
     where summer = (+) . fourth
           mapper (_,_,team,atBats) = applyDatum team atBats
-          totaler (players,_,_) = (+) players
+          totaler (players,_) = (+) players
+
+meanFromSum elements sum = sum / elements
+varFromSums elements sum sumSquared = (sumSquared - (sum^2)) / (elements - 1)
+varMeanFromSums n sum sumSq = (meanFromSum n sum,varFromSums n sum sumSq)
+varMeanFromPair :: (Floating b) => b -> (b,b) -> (b,b)
+varMeanFromPair n (sum,sumSq) = varMeanFromSums n sum sumSq
+
+relativeProb :: (Floating a) => a -> a -> a
+relativeProb elements allElements = elements / allElements
+
+pdfNorm :: (Floating a) => a -> (a,a) -> a
+pdfNorm val (mean, var) = firstFac * expFac
+    where firstFac = 1 / (sqrt ( 2 * pi * var))
+          expFac   = exp ((-(val-mean) ** 2) / (2*var))
+
+cleanTuple :: (Integral b, Num d) => (a,(b,c)) -> (a,d,c)
+cleanTuple (a,(b,c)) = (a,fromIntegral b,c)
+
+statifyOne :: (Floating a, Integral b) => a -> (c,b,[(t,t)]) -> (c,a,[(t,t)])
+statifyOne n (a,k,l) = (a,(fromIntegral k)/n,l)
+
+statifyTwo :: (Floating a, Integral b) => a -> (c,a,[(b,b)]) -> (c,a,[(a,a)])
+statifyTwo n (a,b,l) = 
+    (a,b,(fmap (varMeanFromPair n) (map (\(x,y) -> (fromIntegral x, fromIntegral y)) l)))
+
+statify n = (statifyTwo n) . (statifyOne n)
+
+type Statistics = (Float, Float) -- (mean, var)
+type Classifier = (BL.ByteString, Float, [Statistics])
+
+-- Frustrated by the tools for maps. This seems like a pretty simple function to
+-- want but it was completely absent. Maybe it has something to do with purity?
+-- like, this is pretty much just mutating the data inside the map.
+-- Maybe that means I should be doing this some other way?
+applyToKey :: (Ord k) => (a -> Maybe b -> b) -> k -> a -> M.Map k b -> M.Map k b
+applyToKey func key val map = M.insert key (func val (M.lookup key map)) map
+
+-- There must to be a better way to of be doing this
+statter :: (Integral a, Integral b) => a -> Maybe (a,[(b,b)]) -> (a,[(b,b)])
+statter val Nothing = (1,[(vali,vali^2)])
+    where vali = fromIntegral val
+statter val (Just (num,[(sum,sumsq)])) = (num+1,[(sum+vali,sumsq+vali^2)])
+    where vali = fromIntegral val
+
+-- Jesus christ this situation is gross.
+-- I'm doing it in this assbackwards way because I have this idea that
+-- maybe it should be generalizable, so the functions need to give a list of
+-- sufficient statistics for each attribute and also the total number of elements.
+--
+-- For this case we only have one interesting attribute, but if we were to do
+-- more this should be workable using this method.
+
+applyDatum ::
+    (Ord k, Integral a, Integral b) => k -> a -> M.Map k (a,[(b,b)]) -> M.Map k (a,[(b,b)])
+applyDatum = applyToKey statter
+
+----------------------------
+-- NAIVE BAYES CLASSIFIER --
+----------------------------
+
+-- Helper for selector
+maxPairr :: (Ord a) => (b, a) -> (b, a) -> (b, a)
+maxPairr (x, a) (y, b)
+    | a > b     = (x, a)
+    | a < b     = (y, b)
+    | otherwise = (x, a)
+
+-- Selects the classifier with the greatest pdfthing from a list of them
+selector :: (Foldable f) => f (s, Float) -> s
+selector pairs = fst (foldl1 maxPairr pairs)
+
+--normFold :: (Functor f) => Classifier -> f Float -> (String, Float)
+--normFold (cls, prob, attstats) atts = (cls, normed)
+--    where normed = foldl (*) (zipWith pdfNorm atts attstats) prob
+
+normFold :: [Float] -> Classifier -> (BL.ByteString, Float)
+normFold atts (cls, prob, attstats) = (cls, normed)
+    where normed = foldl (*) prob (zipWith pdfNorm atts attstats)
+
+-- Given a list of classifiers and datum, will attempt to classify the datum.
+bayes :: (Functor f, Foldable f) => f Classifier -> [Float] -> BL.ByteString
+bayes classes atts = selector (fmap (normFold atts) classes)
+
+
+
+-- Old stuff I no longer need for this project.
 
 -- No longer needed!
 uniqueAdd :: (Eq a, Ord a) => a -> [a] -> [a]
@@ -35,7 +132,7 @@ uniqueAdd item aList
     | otherwise         = item : aList
 
 -- A little search into the docs indicates that this is already done by a function
--- called genericLength. Strange.
+-- called genericLength.
 nLength :: (Foldable t, Num b) => t a -> b
 nLength = fromIntegral . length
 
@@ -53,93 +150,6 @@ variance xs = ((expectedSquared) - (meanSquared)) / (nLength xs - 1)
     where expectedSquared = sum (fmap (\x -> x ** 2) xs)
           meanSquared = nLength xs * (mean xs) ** 2
 
-meanFromSum :: (Fractional a) => a -> a -> a
-meanFromSum elements sum = sum / elements
--- This ought to be equivalent.
-varFromSums :: (Fractional a) => a -> a -> a -> a
-varFromSums elements sum sumSquared = (sumSquared - (sum^2)) / (elements - 1)
-
-relativeProb :: (Fractional a) => a -> a -> a
-relativeProb elements allElements = elements / allElements
-
 meanVar :: (Floating a, Functor f, Foldable f) => f a -> (a,a)
 meanVar xs = (mean xs, variance xs)
 
-pdfNorm :: (Floating a) => a -> (a,a) -> a
-pdfNorm val (mean, var) = firstFac * expFac
-    where firstFac = 1 / (sqrt ( 2 * pi * var))
-          expFac   = exp ((-(val-mean) ** 2) / (2*var))
-
--- Type names so I understand what I'm doing
--- Or maybe it's so I dont understand
--- Does abstraction promote understanding or reduce it?
---
--- This should all have been done using Data! Whoops!
--- Would have been very convenient
-type Statistics     = (Float, Float) -- (Mean, Var)
-type AttributeData  = [Float]
-type ClassifierData = (String, Float, [AttributeData])
-type Classifier     = (String, Float, [Statistics])
-
-classifierStats :: ClassifierData -> Classifier
-classifierStats (cls, prob, atts) = (cls, prob, statCalc)
-    where statCalc= fmap meanVar atts
-
--- Helper for selector
-maxPairr :: (Ord a) => (b, a) -> (b, a) -> (b, a)
-maxPairr (x, a) (y, b)
-    | a > b     = (x, a)
-    | a < b     = (y, b)
-    | otherwise = (x, a)
-
--- Selects the classifier with the greatest pdfthing from a list of them
-selector :: (Foldable f) => f (String, Float) -> String
-selector pairs = fst (foldl1 maxPairr pairs)
-
---normFold :: (Functor f) => Classifier -> f Float -> (String, Float)
---normFold (cls, prob, attstats) atts = (cls, normed)
---    where normed = foldl (*) (zipWith pdfNorm atts attstats) prob
-
-normFold :: [Float] -> Classifier -> (String, Float)
-normFold atts (cls, prob, attstats) = (cls, normed)
-    where normed = foldl (*) prob (zipWith pdfNorm atts attstats)
-
--- Given a list of classifiers and datum, will attempt to classify the datum.
-bayes :: (Functor f, Foldable f) => f Classifier -> [Float] -> String
-bayes classes atts = selector (fmap (normFold atts) classes)
-
--- Used for constructing (key, AttributeData) pairs.
-lister :: Ord k => k -> [a] -> [a] -> [a]
-lister key newval oldval = (head newval) : oldval 
-
--- Frustrated by the tools for maps. This seems like a pretty simple function to
--- want but it was completely absent. Maybe it has something to do with purity?
--- like, this is pretty much just mutating the data inside the map.
--- Maybe that means I should be doing this some other way?
-applyToKey :: (Ord k) => (a -> Maybe b -> b) -> k -> a -> M.Map k b -> M.Map k b
-applyToKey func key val map = M.insert key (func val (M.lookup key map)) map
-
-type Sums = (Int,[(Integer,Integer)]) -- May be sums, may be complete statistics
--- This is pretty irritating because it's
-
--- Problem with this is that sum squared is at risk of overflow.
--- Running means are smaller, but introduce uncertainty.
---
--- Further research indicates that haskell automatically hangles large numbers
--- deal when necessary, but this will probably result in performance losses.
-statter :: (Integral a) => a -> Maybe Sums -> Sums
-statter val Nothing = (1,[(vali,vali^2)])
-    where vali = fromIntegral val
-statter val (Just (num,[(sum,sumsq)])) = (num+1,[(sum+vali,sumsq+vali^2)])
-    where vali = fromIntegral val
-
--- Jesus christ this situation is gross.
--- I'm doing it in this assbackwards way because I have this idea that
--- maybe it should be generalizable, so the functions need to give a list of
--- sufficient statistics for each attribute and also the total number of elements.
---
--- For this case we only have one interesting attribute, but if we were to do
--- more this should be workable using this method.
-
-applyDatum :: (Ord k, Integral a) => k -> a -> M.Map k Sums -> M.Map k Sums
-applyDatum = applyToKey statter
