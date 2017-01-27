29a94b4d6e319f62632344eed7b766ff1a877765
David Shank <shank.david@gmail.com>
2017-01-27 11:54:39 -0500

    Cleaning code

--- naive-bayes/src/Main.hs
+++ naive-bayes/src/Main.hs
@@ -21,81 +21,53 @@ main :: IO ()
 main = do
     csvData <- BL.readFile "batting.csv"
     let summed = F.foldr summer 0 (baseballStats csvData)
-    let sums = F.foldr mapper M.empty (baseballStats csvData) ::
-                            M.Map BL.ByteString (Int, [(Integer,Integer)])
+    let sums = F.foldr mapper M.empty (baseballStats csvData)
     let total = foldr totaler 0 sums
     let list = M.toList sums
     --putStrLn $ "preCleaning " ++ (show list)
     let cleanList = fmap cleanTuple list
     --putStrLn $ "stuffs " ++ (show cleanList)
-    let statList = map (statifyCorrect (fromIntegral total)) cleanList
+    let statList = map (sumsToStats (fromIntegral total)) cleanList
     --putStrLn $ "these numbers " ++ (show statList)
-    let cls = bayes statList [1000000]
+    let cls1 = bayes statList [1000000]
+    let cls2 = bayes statList [1]
+    let cls3 = bayes statList [200]
     --let normFolded = fmap (normFold [50]) statList
     --putStrLn $ "after norming " ++ (show normFolded)
     
     --putStrLn $ "Total atBats was: " ++ (show summed)
-    putStrLn $ "team was" ++ (show cls)
+    putStrLn $ "team was" ++ (show cls1) ++ (show cls2) ++ (show cls3)
     where summer = (+) . fourth
           mapper (_,_,team,atBats) = applyDatum team atBats
           totaler (players,_) = (+) players
 
-meanFromSum elements sum = sum / elements
+---------------------
+-- FORMATTING DATA --
+---------------------
 
-
-varFromSums elements sum sumSquared = (sumSquared - (sum^2)/elements) / (elements - 1)
-
-varMeanFromSums :: (Floating b, Integral a) => b -> a -> a -> (b,b)
-varMeanFromSums n sum sumSq = 
-    (meanFromSum n (fromIntegral sum),varFromSums n (fromIntegral sum) (fromIntegral sumSq))
-
-varMeanFromPair :: (Floating b, Integral a) => b -> (a,a) -> (b,b)
-varMeanFromPair n (sum,sumSq) = varMeanFromSums n sum sumSq
-
-relativeProb :: (Floating a) => a -> a -> a
-relativeProb elements allElements = elements / allElements
-
-pdfNorm :: (Floating a) => a -> (a,a) -> a
-pdfNorm val (mean, var) = firstFac * expFac
-    where firstFac = 1 / (sqrt ( 2 * pi * var))
-          expFac   = exp ((-(val-mean) ** 2) / (2*var))
-
--- Special function for cleaning the tuples produced in this
+-- Special function for "cleaning" the tuples produced in this
 cleanTuple :: (Integral b, Num d) => (a,(b,c)) -> (a,d,c)
 cleanTuple (a,(b,c)) = (a,fromIntegral b,c)
 
 cleanList :: (Integral b, Num d) => [(a,(b,c))] -> [(a,d,c)]
 cleanList = map cleanTuple
 
-type BS = BL.ByteString
-statifyCorrect :: (Floating a, Integral b) => a -> (BS, a, [(b,b)]) -> (BS, a, [(a,a)])
-statifyCorrect tot (cls, num, prb) = (cls, num/tot, (map (varMeanFromPair num) prb))
+sumsToStats :: (Floating a, Integral b) => a -> (c, a, [(b,b)]) -> (c, a, [(a,a)])
+sumsToStats tot (cls, num, prb) = (cls, num/tot, (map (varMeanFromPair num) prb))
 
--- Frustrated by the tools for maps. This seems like a pretty simple function to
--- want but it was completely absent. Maybe it has something to do with purity?
--- like, this is pretty much just mutating the data inside the map.
--- Maybe that means I should be doing this some other way?
 applyToKey :: (Ord k) => (a -> Maybe b -> b) -> k -> a -> M.Map k b -> M.Map k b
 applyToKey func key val map = M.insert key (func val (M.lookup key map)) map
 
--- There must to be a better way to of be doing this
-statter :: (Integral a, Integral b) => a -> Maybe (a,[(b,b)]) -> (a,[(b,b)])
-statter val Nothing = (1,[(vali,vali^2)])
-    where vali = fromIntegral val
-statter val (Just (num,[(sum,sumsq)])) = (num+1,[(sum+vali,sumsq+vali^2)])
-    where vali = fromIntegral val
-fI = fromIntegral
--- Jesus christ this situation is gross.
--- I'm doing it in this assbackwards way because I have this idea that
--- maybe it should be generalizable, so the functions need to give a list of
--- sufficient statistics for each attribute and also the total number of elements.
---
--- For this case we only have one interesting attribute, but if we were to do
--- more this should be workable using this method.
+sumAndSqSum :: Num a => a -> (a,a) -> (a,a)
+sumAndSqSum num (oldSum, oldSqSum) = (oldSum + num, oldSqSum + num^2)
+
+summer :: (Num a) => a -> Maybe (a,[(a,a)]) -> (a,[(a,a)])
+summer val Nothing = (1,[(val,val^2)])
+summer val (Just (num,list)) = (num+1,fmap (sumAndSqSum val) list)
 
 applyDatum ::
-    (Ord k, Integral a, Integral b) => k -> a -> M.Map k (a,[(b,b)]) -> M.Map k (a,[(b,b)])
-applyDatum = applyToKey statter
+    (Ord k, Num a) => k -> a -> M.Map k (a,[(a,a)]) -> M.Map k (a,[(a,a)])
+applyDatum = applyToKey summer
 
 ----------------------------
 -- NAIVE BAYES CLASSIFIER --
@@ -103,6 +75,9 @@ applyDatum = applyToKey statter
 
 type Statistics = (Float, Float) -- (mean, var)
 type Classifier a = (a, Float, [Statistics])
+-- Wow! Type inference is amazing. I assumed that because I added a type parameter it
+-- would need to be specified everywhere but apparently it _just works_
+-- I should still maybe consider making this a data type
 
 -- Helper for selector
 maxPairr :: (Ord a) => (b, a) -> (b, a) -> (b, a)
@@ -115,16 +90,33 @@ maxPairr (x, a) (y, b)
 selector :: (Foldable f) => f (s, Float) -> s
 selector pairs = fst (foldl1 maxPairr pairs)
 
+pdfNorm :: (Floating a) => a -> (a,a) -> a
+pdfNorm val (mean, var) = firstFac * expFac
+    where firstFac = 1 / (sqrt ( 2 * pi * var))
+          expFac   = exp ((-(val-mean) ** 2) / (2*var))
+
 normFold :: [Float] -> Classifier a -> (a, Float)
 normFold atts (cls, prob, attstats) = (cls, normed)
     where normed = foldl (*) prob (zipWith pdfNorm atts attstats)
-
 
 -- Given a list of classifiers and datum, will attempt to classify the datum.
 bayes :: (Functor f, Foldable f) => f (Classifier a) -> [Float] -> a
 bayes classes atts = selector (fmap (normFold atts) classes)
 
 
-
 mean :: (Floating a, Functor f, Foldable f) => f a -> a
 mean xs = (sum xs) / (fromIntegral (length xs))
+
+-----------------
+-- PROBABILITY --
+-----------------
+
+meanFromSum elements sum = sum / elements
+varFromSums elements sum sumSquared = (sumSquared - (sum^2)/elements) / (elements - 1)
+
+varMeanFromSums :: (Floating b, Integral a) => b -> a -> a -> (b,b)
+varMeanFromSums n sum sumSq = 
+    (meanFromSum n (fromIntegral sum),varFromSums n (fromIntegral sum) (fromIntegral sumSq))
+
+varMeanFromPair :: (Floating b, Integral a) => b -> (a,a) -> (b,b)
+varMeanFromPair n (sum,sumSq) = varMeanFromSums n sum sumSq
